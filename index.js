adba7741508311f4a396e29081b374ebc958d442
mounicakadaagunta <mounicakadapagunta13@gmail.com>
2022-04-10 00:06:11 +0530

    put,delete and dotenv

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // const express = require("express");// "type':"commonjs"
 import express from "express"; //"type":"module"
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 
 const PORT = 5000;
@@ -10,7 +13,7 @@ app.use(express.json());
 //every request in the app body is parsed as json
 
 //mongodb connection settings
-const MONGO_URL = "mongodb://localhost";
+const MONGO_URL = process.env.MONGO_URL;
 async function createConnection() {
     const client = new MongoClient(MONGO_URL);
     await client.connect(); //returns a promise object
@@ -27,58 +30,94 @@ app.get("/", (request, response) => {
 app.get("/movies", async (request, response) => {
     //request-> query params
     console.log(request.query);
-
     //to filter the movie as per condition
     const filter = request.query;
     console.log(filter)
     if (filter.rating) {
-        filter.rating = parseInt(filter.rating)
+        filter.rating = +filter.rating
     }
-    // const { language, rating } = request.query
-    // console.log(language, rating);
-    // let filterMovies = movies
-    // if (language) {
-    //     filterMovies = movies.filter((mv) =>
-    //         mv.language === language);
-    // }
-    // if (rating) {
-    //     filterMovies = movies.filter((mv) =>
-    //         mv.rating === +rating);
-    // }
-    const filterMovies = await client
-        .db("b28wd")
-        .collection("movies")
-        .find({})
-        .toArray()
+
+    const filterMovies = await getMovies()
     response.send(filterMovies);
 });
 app.post("/movies", async (request, response) => {
     const data = request.body;
     //console.log(data);
-    const result = await client
-        .db("b28wd")
-        .collection("movies")
-        .insertMany(data);
+    const result = await createMovies(data);
     response.send(result);
 })
 
+// to get movie by id 
 app.get("/movies/:id", async (request, response) => {
     console.log(request.params);
     const { id } = request.params;
-
-    const movie = await client
-        .db("b28wd")
-        .collection("movies")
-        .findOne({ id: id })
+    const movie = await getMovieById(id)
     // const movie = movies.find((mv) => mv.id === id);
     console.log(movie);
-    //when there is no movie matching with the id 
-    // display no matching movies on the screen 
     movie
         ? response.send(movie)
         : response.status(404)
             .send({ message: "No matching movie found" });
 });
 
+//to delete movie
+app.delete("/movies/:id", async (request, response) => {
+    console.log(request.params);
+    const { id } = request.params;
+    const result = await deleteMovieById(id);
+    result.deletedCount > 0
+        ? response.send(result)
+        : response.status(404)
+            .send({ message: "No matching movie found" });
+});
+//to update the movie list
+app.put("/movies/:id", async (request, response) => {
+    console.log(request.params);
+    const { id } = request.params;
+
+    const data = request.body;
+    const result = await updateMovieById(id, data);
+    const movie = await getMovieById(id)
+
+});
+
 app.listen(PORT, () =>
     console.log("App is started in ", PORT));
+
+
+async function updateMovieById(id, data) {
+    return await client
+        .db("b28wd")
+        .collection("movies")
+        .updateOne({ id: id }, { $set: data });
+}
+
+async function createMovies(data) {
+    return await client
+        .db("b28wd")
+        .collection("movies")
+        .insertMany(data);
+}
+
+async function getMovies() {
+    return await client
+        .db("b28wd")
+        .collection("movies")
+        .find({})
+        .toArray();
+}
+
+async function deleteMovieById(id) {
+    return await client
+        .db("b28wd")
+        .collection("movies")
+        .deleteOne({ id: id });
+}
+
+async function getMovieById(id) {
+    return await client
+        .db("b28wd")
+        .collection("movies")
+        .findOne({ id: id });
+}
+
