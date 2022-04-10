b32d0d99084d15403447a4d57672844a5b66ddf0
mounicakadaagunta <mounicakadapagunta13@gmail.com>
2022-04-10 16:37:40 +0530

    objectid updated

--- helper.js
+++ helper.js
@@ -1,10 +1,11 @@
 import { client } from "./index.js";
-
+import { ObjectId } from "mongodb"
 async function updateMovieById(id, data) {
     return await client
         .db("b28wd")
         .collection("movies")
-        .updateOne({ id: id }, { $set: data });
+        .updateOne({ _id: ObjectId(id) },
+            { $set: data });
 }
 async function createMovies(data) {
     return await client
@@ -12,24 +13,24 @@ async function createMovies(data) {
         .collection("movies")
         .insertMany(data);
 }
-async function getMovies() {
+async function getMovies(filter) {
     return await client
         .db("b28wd")
         .collection("movies")
-        .find({})
+        .find(filter)
         .toArray();
 }
 async function deleteMovieById(id) {
     return await client
         .db("b28wd")
         .collection("movies")
-        .deleteOne({ id: id });
+        .deleteOne({ _id: ObjectId(id) });
 }
 async function getMovieById(id) {
     return await client
         .db("b28wd")
         .collection("movies")
-        .findOne({ id: id });
+        .findOne({ _id: ObjectId(id) });
 }
 
 export {
